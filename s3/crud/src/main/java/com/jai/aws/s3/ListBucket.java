85cbe0360a3ed63a768fb8f26e3ba7d98b21ad68
Jayakumar Jayaraman <india.jai@gmail.com>
2019-04-05 22:24:20 +0100

    updated

--- s3/crud/src/main/java/com/jai/aws/s3/ListBucket.java
+++ s3/crud/src/main/java/com/jai/aws/s3/ListBucket.java
@@ -1,4 +1,23 @@
 package com.jai.aws.s3;
 
+
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.Bucket;
+import software.amazon.awssdk.services.s3.model.ListBucketsResponse;
+
+import java.util.List;
+
 public class ListBucket {
+
+    public static void main(String[] args) {
+        S3Client s3Client = S3Client.builder().region(Region.EU_WEST_1).build();
+        ListBucketsResponse response = s3Client.listBuckets();
+
+        List<Bucket> buckets = response.buckets();
+
+        for (Bucket bucket : buckets) {
+            System.out.println(bucket.name());
+        }
+    }
 }
