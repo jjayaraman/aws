d09b4b4988a55846f5d13ead5a22b5bd69cceb48
jjayaraman <india.jai@gmail.com>
2019-06-28 07:02:20 +0100

    sdk 1.11 code added

---
+++ s3/aws_sdk_1_11/src/main/java/com/jai/aws/s3/ListBuckets.java
@@ -0,0 +1,23 @@
+package com.jai.aws.s3;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.Bucket;
+
+import java.util.List;
+
+public class ListBuckets {
+
+    public static void main(String[] args) {
+
+        final AmazonS3 s3 = AmazonS3ClientBuilder.defaultClient();
+        List<Bucket> buckets = s3.listBuckets();
+        System.out.println("Your Amazon S3 buckets are:");
+        for (Bucket b : buckets) {
+            System.out.println("* " + b.getName());
+
+            System.out.println("* " + b.);
+        }
+
+    }
+}
